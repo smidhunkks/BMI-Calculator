bf9c6b664e83bc9149014d476d3ee5e8743fd69a
midhun <jmsmidhunkrishna@gmail.com>
2020-05-22 20:51:02 +0530

    enthakuvo entho

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,15 +9,51 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                )),
+                Expanded(
+                    child: ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                )),
+              ],
+            )),
+            Expanded(child: ReusableCard(colour: Color(0xFF1D1E33))),
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33))),
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33))),
+              ],
+            )),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: colour, borderRadius: BorderRadius.circular(10.0)),
+      height: 200.0,
+      width: 170.0,
     );
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,9 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21)
-      ),
+          primaryColor: Color(0xFF0A0E21),
+          scaffoldBackgroundColor: Color(0xFF0A0E21)),
       home: InputPage(),
     );
   }
 }
-
-
